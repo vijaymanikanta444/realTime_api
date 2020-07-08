dfca99a1c9dfb5926b26b69a7a599c39ef66767f
vijaymanikanta444 <vijayakp444@gmail.com>
2020-07-09 01:34:05 +0530

    added goodreads api

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,32 +1,55 @@
 import express from 'express';
+import request from 'request-promise';
+import { parseString } from 'xml2js';
+import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
+router.use(authenticate);
 
 router.get('/search', (req, res) => {
-  res.json({
-    books: [
-      {
-        goodreadsId: 1,
-        title: '1984',
-        authors: 'orwell',
-        covers: [
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348990566l/5470.jpg',
-          'https://i.pinimg.com/236x/0a/bf/93/0abf93ca1041823138216ca5fdc67f70--george-orwell-big-brothers.jpg',
-        ],
-        pages: 198,
-      },
-      {
-        goodreadsId: 2,
-        title: 'Three men in a boat',
-        authors: 'vijay',
-        covers: [
-          'https://images.gr-assets.com/books/139264578378/4921.jpg',
-          'https://images.gr-assets.com/books/1536846565378/2476823.jpg',
-        ],
-        pages: 123,
-      },
-    ],
-  });
+  request
+    .get(
+      `https://www.goodreads.com/search/index.xml?key=2hQXKL4ciq2rLS3Ld3w&q=${req.query.q}`
+    )
+    .then((result) =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            (work) => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].image_url[0]],
+            })
+          ),
+        })
+      )
+    );
+
+  // res.json({
+  //   books: [
+  //     {
+  //       goodreadsId: 1,
+  //       title: '1984',
+  //       authors: 'orwell',
+  //       covers: [
+  //         'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348990566l/5470.jpg',
+  //         'https://i.pinimg.com/236x/0a/bf/93/0abf93ca1041823138216ca5fdc67f70--george-orwell-big-brothers.jpg',
+  //       ],
+  //       pages: 198,
+  //     },
+  //     {
+  //       goodreadsId: 2,
+  //       title: 'Three men in a boat',
+  //       authors: 'vijay',
+  //       covers: [
+  //         'https://images-na.ssl-images-amazon.com/images/I/91tkXfaaBCL.jpg',
+  //         'https://m.media-amazon.com/images/I/51HF1rglegL.jpg',
+  //       ],
+  //       pages: 123,
+  //     },
+  //   ],
+  // });
 });
 
 export default router;
